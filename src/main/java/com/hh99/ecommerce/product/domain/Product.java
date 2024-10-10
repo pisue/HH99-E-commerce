402ce197181fe894f6f4921287c6ed0f041d98c8
pisue22 <pisue22@naver.com>
2024-10-10 21:05:10 +0900

    feat:Product Mock API

---
+++ src/main/java/com/hh99/ecommerce/product/domain/Product.java
@@ -0,0 +1,31 @@
+package com.hh99.ecommerce.product.domain;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+
+    @Column(name = "reg_date", nullable = false)
+    private LocalDateTime regDate;
+
+}
