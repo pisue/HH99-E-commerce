16eb8954cfb225e38df4cc2d11491c538761678c
pisue <pisue22@naver.com>
2024-10-29 00:12:23 +0900

    fix: 주문 CreateOrderRequest 수정

--- src/main/java/com/hh99/ecommerce/order/interfaces/request/CreateOrderRequest.java
+++ src/main/java/com/hh99/ecommerce/order/interfaces/request/CreateOrderRequest.java
@@ -2,14 +2,14 @@ package com.hh99.ecommerce.order.interfaces.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+import java.util.List;
+
+@Getter
 public class CreateOrderRequest {
     private Long userId;
-    private Long productId;
-    private int quantity;
+    private List<ProductOrderRequest> productOrderRequests;
 }
 
