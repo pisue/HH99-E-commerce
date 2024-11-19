0e9bd7f04a38991dd4614b7b68605a03a70d6855
pisue <pisue22@naver.com>
2024-11-19 17:39:41 +0900

    fix: Order Minor Changes - table명 orders로 변경

--- src/main/java/com/hh99/ecommerce/order/infra/entity/Order.java
+++ src/main/java/com/hh99/ecommerce/order/infra/entity/Order.java
@@ -8,7 +8,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "`order`")
+@Table(name = "orders")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Order {
